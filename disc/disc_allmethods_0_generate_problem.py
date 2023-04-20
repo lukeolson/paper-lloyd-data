018a38b4f275a3050258cb5da1c5d7c7760b944e
Luke Olson <luke.olson@gmail.com>
2023-04-20 12:46:46 -0600

    load static data

--- disc/disc_allmethods_0_generate_problem.py
+++
@@ -1,6 +0,0 @@
-"""Compare standard to lloyd to metis to rb lloyd methods."""
-import numpy as np
-from disc_generate import gen_A
-
-A, mesh, bc = gen_A(500, remove_dirichlet=False)
-np.savez('disc_allmethods_0_output.npz', A=A, V=mesh.V, E=mesh.E)
